b75abad030eae8880171d89a98278d4697831be6
Lev <lev-sidorov@yandex.ru>
2022-04-06 02:36:11 +0700

    Add range for Limit property.

--- View/Recipes/RecipeSearchInfo.cs
+++ View/Recipes/RecipeSearchInfo.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace View.Recipes
 {
     public class RecipeSearchInfo
     {
         public string Name { get; set; }
+
         public string Cuisine { get; set; }
+
         public string Category { get; set; }
+
         public DateTime? FromCreatedAt { get; set; }
+
         public DateTime? ToCreatedAt { get; set; }
-        public int? Limit { get; set; } = 100;
+
+        [Range(1, 25)]
+        public int? Limit { get; set; } = 5;
+
         public int? Offset { get; set; } = 0;
     }
 }
