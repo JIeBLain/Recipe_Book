73637d1c0fc0478c23811f93fd4bcae38be4c2a6
Lev <lev-sidorov@yandex.ru>
2022-04-06 02:21:21 +0700

    Add IRecipesService.cs

---
+++ API/Recipes/IRecipesService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ViewRecipes = View.Recipes;
+
+namespace RecipesBook.Recipes
+{
+    public interface IRecipesService
+    {
+        public Task<ViewRecipes.Recipe> GetRecipeAsync(string id, CancellationToken token);
+
+        public Task<ViewRecipes.RecipesList> SearchRecipesAsync(
+            ViewRecipes.RecipeSearchInfo viewSearchInfo, CancellationToken token);
+
+        public Task<ViewRecipes.Recipe> CreateRecipeAsync(
+            ViewRecipes.RecipeCreateInfo viewCreateInfo, CancellationToken token);
+
+        public Task UpdateRecipeAsync(string id,
+            ViewRecipes.RecipeUpdateInfo viewUpdateInfo, CancellationToken token);
+
+        public Task DeleteRecipeAsync(string id, CancellationToken token);
+
+        public Task DeleteAllRecipesAsync(CancellationToken token);
+    }
+}
